a3696be061e9e6afa30cf70529dbd8bb43a6cf57
Amanevr <92138410+Amanevr@users.noreply.github.com>
2021-11-12 17:37:04 +0530

    Update urls.py

--- Doctor/urls.py
+++ Doctor/urls.py
@@ -1,16 +1,1 @@
-"""from django.urls import path
-from . import views
 
-urlpatterns = [
-    path('', views.index),
-    path('<str:client_num>/', views.detail),
-]"""
-
-from django.urls import path
-from .views import HomeTemplateView, AppointmentTemplateView, ManageAppointmentTemplateView
-
-urlpatterns = [
-    path("", HomeTemplateView.as_view(), name="home"),
-    path("make-an-appointment/", AppointmentTemplateView.as_view(), name="appointment"),
-    path("manage-appointments/", ManageAppointmentTemplateView.as_view(), name="manage"),
-]
